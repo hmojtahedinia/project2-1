17964172ba7c4e7b76537c3b477109af9663f9df
Treudey <alextreude@hotmail.com>
2019-05-09 23:01:37 -0400

    got map fully integrated with front end and database

--- models/Washroom.js
+++ models/Washroom.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
     var Washroom = sequelize.define("Washroom", {
       nameOfPlace: DataTypes.STRING,
       address: DataTypes.STRING,
-      overalRating: DataTypes.INTEGER,
+      overallRating: DataTypes.INTEGER,
       comment: DataTypes.TEXT
     });
     return Washroom;
--- models/seeds.sql
+++ models/seeds.sql
@@ -1,25 +1,25 @@
 USE washroomDb;
 
-INSERT INTO ratingsTable (
-id, nameOfPlace, address, overalRating, comment)
-VALUES (1, 'Kinton Ramen', '90 Eglinton Ave. East, Unit 108', 7, 'The soap seemed to be very watered down. Bonus points for the complimentary mouth wash and tooth picks. Would definitely poop again.'),
-		(2, 'Han Ba Tang', '4862 Yonge Street', 7, ' I found it very weird that the inside of the stalls did not match the same rustic wood panelling as the rest of the bathroom.'),
-        (3, 'Pancho y Emiliano', '291 King Street West', 7, 'Absolutely spotless, apart from single piece of toilet paper next to the toilet. Loved the subway tiles and the vintage hand dryer. Bonus points for having both paper towel and hand dryer. Would definitely poop again.'),
-        (4, 'ZED 80', '185 Danforth Avenue ', 7, 'There was a bag of clothes next to the toilet the entire night. Bonus points for having both paper towel and hand dryer. Bonus, bonus points for Garbage Pail Kids! '),
-        (5, 'Air Canada Centre', '40 Bay Street', 6, 'Though loss for the Raptors tonight. The smell of urine and overpriced beer permeated the air. Vanity covered in water. Urinals, urinals everywhere.'),
-        (6, 'Glass Onion Bistro', '1316 Bloor Street West', 6, 'The faint sound of a cat meowing was heard. A search of the basement was conducted to find the source, but to no avail. Decent bathroom.'),
-        (7, 'Toys "R" Us (Dufferin Mall)', '900 Dufferin Street', 6, 'I"m going to assume that it was just cleaned before I got there because I"ve heard horror stories about this particular bathroom. There was an unpleasant lingering smell in the air (most definitely from the employee that was exiting as I was entering).'),
-        (8, 'Cider House', '391 Roncesvalles Avenue', 7, 'Private poop closet. Urinal was out of order.'),
-        (9, 'The Rhino Bar & Grill', '1249 Queen Street West', 5, 'A typical Saturday night at The Rhino.'),
-        (10, 'See-Scape', '347 Keele Street', 5, 'The Punisher display was pretty cool. Other than that, not much to say about this bathroom.'),
-        (11, 'NAAN & KABO', '691 Yonge Street', 7, 'PLEASE DO NOT THROW PAPER TOWELS IN THE TOILET'),
-        (12, 'Barque Smokehouse', '299 Roncesvalles Ave.', 8, 'Four bathrooms, four different layouts. One of which was wheelchair accessible. Additional sink just outside bathroom area for those who want to wash their hands after eating some good BBQ.'),
-        (13, 'Smoque N" Bones', '869 Queen Street West', 7, 'INCREDIBLE BBQ! Decent bathroom. The only thing of note was the caged off wall of booze right outside the bathrooms. Pretty cool.'),
-        (14, 'Dufferin Mall', '900 Dufferin St', 6, 'The smell hit me as soon as I walked in. Water pressure of sinks was worst I"ve encountered yet. Took me a good 5 minutes to wash all of the soap off my hands. Speaking of soap, it had a weird smell to it. Not a good smell, but not bad either.'),
-        (15, 'Kramer"s Bar & Grill', '1915 Yonge Street', 7, 'The Jack Daniel"s soap dispensers were super cool. Decent bar bathroom with option of paper towel or hand dryer.'),
-        (16, 'Loblaws', '12 St Clair Ave East', 7, 'Loved the red floor to ceiling tiles. Very spacious. Soap left a very strong (chemically) smell on my hands. Not bad for a grocery store. Bonus points for hand sanitizer dispenser just outside the door.'),
-        (17, 'Cluny Bistro & Boulangerie', '35 Tank House Lane', 7, '35 Tank House Lane'),
-        (18, 'The Office Pub', '7 King Street East', 7, 'Went on a Friday night so bathroom was a mess. Pro tip: when paper towel not available, use toilet paper to open door.'),
-        (19, 'Berkeley Church,', '315 Queen Street East', 7, 'Beautiful venue, nice bathroom (2 stalls, 3 urinals, 2 sinks, full length mirror, etc.)'),
-        (20, 'Beach Blast', '15 Leswyn Road', 6, 'As a person who dislikes the beach, this place was not my cup of tea. There was sand EVERYWHERE!')
-        
+INSERT INTO washrooms (nameOfPlace, address, overalRating, comment)
+VALUES ('Kinton Ramen', '90 Eglinton Ave. East, Unit 108', 7, 'The soap seemed to be very watered down. Bonus points for the complimentary mouth wash and tooth picks. Would definitely poop again.'),
+		('Han Ba Tang', '4862 Yonge Street', 7, ' I found it very weird that the inside of the stalls did not match the same rustic wood panelling as the rest of the bathroom.'),
+        ('Pancho y Emiliano', '291 King Street West', 7, 'Absolutely spotless, apart from single piece of toilet paper next to the toilet. Loved the subway tiles and the vintage hand dryer. Bonus points for having both paper towel and hand dryer. Would definitely poop again.'),
+        ('ZED 80', '185 Danforth Avenue ', 7, 'There was a bag of clothes next to the toilet the entire night. Bonus points for having both paper towel and hand dryer. Bonus, bonus points for Garbage Pail Kids! '),
+        ('Air Canada Centre', '40 Bay Street', 6, 'Though loss for the Raptors tonight. The smell of urine and overpriced beer permeated the air. Vanity covered in water. Urinals, urinals everywhere.'),
+        ('Glass Onion Bistro', '1316 Bloor Street West', 6, 'The faint sound of a cat meowing was heard. A search of the basement was conducted to find the source, but to no avail. Decent bathroom.'),
+        ('Toys "R" Us (Dufferin Mall)', '900 Dufferin Street', 6, 'I"m going to assume that it was just cleaned before I got there because I"ve heard horror stories about this particular bathroom. There was an unpleasant lingering smell in the air (most definitely from the employee that was exiting as I was entering).'),
+        ('Cider House', '391 Roncesvalles Avenue', 7, 'Private poop closet. Urinal was out of order.'),
+        ('The Rhino Bar & Grill', '1249 Queen Street West', 5, 'A typical Saturday night at The Rhino.'),
+        ('See-Scape', '347 Keele Street', 5, 'The Punisher display was pretty cool. Other than that, not much to say about this bathroom.'),
+        ('NAAN & KABO', '691 Yonge Street', 7, 'PLEASE DO NOT THROW PAPER TOWELS IN THE TOILET'),
+        ('Barque Smokehouse', '299 Roncesvalles Ave.', 8, 'Four bathrooms, four different layouts. One of which was wheelchair accessible. Additional sink just outside bathroom area for those who want to wash their hands after eating some good BBQ.'),
+        ('Smoque N" Bones', '869 Queen Street West', 7, 'INCREDIBLE BBQ! Decent bathroom. The only thing of note was the caged off wall of booze right outside the bathrooms. Pretty cool.'),
+        ('Dufferin Mall', '900 Dufferin St', 6, 'The smell hit me as soon as I walked in. Water pressure of sinks was worst I"ve encountered yet. Took me a good 5 minutes to wash all of the soap off my hands. Speaking of soap, it had a weird smell to it. Not a good smell, but not bad either.'),
+        ('Kramer"s Bar & Grill', '1915 Yonge Street', 7, 'The Jack Daniel"s soap dispensers were super cool. Decent bar bathroom with option of paper towel or hand dryer.'),
+        ('Loblaws', '12 St Clair Ave East', 7, 'Loved the red floor to ceiling tiles. Very spacious. Soap left a very strong (chemically) smell on my hands. Not bad for a grocery store. Bonus points for hand sanitizer dispenser just outside the door.'),
+        ('Cluny Bistro & Boulangerie', '35 Tank House Lane', 7, '35 Tank House Lane'),
+        ('The Office Pub', '7 King Street East', 7, 'Went on a Friday night so bathroom was a mess. Pro tip: when paper towel not available, use toilet paper to open door.'),
+        ('Berkeley Church,', '315 Queen Street East', 7, 'Beautiful venue, nice bathroom (2 stalls, 3 urinals, 2 sinks, full length mirror, etc.)'),
+        ('Beach Blast', '15 Leswyn Road', 6, 'As a person who dislikes the beach, this place was not my cup of tea. There was sand EVERYWHERE!')
+
+SELECT * FROM washroomdb.washrooms;       
